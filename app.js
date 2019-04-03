91d492e1fac7c1412d303120c5c2a5edca84dfa4
patDevNG <cgrleadership@gmail.com>
2019-04-03 14:02:50 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, ()=>{
+    console.log(`Server Statrted at Port: ${PORT}`);
+    
+});
+
